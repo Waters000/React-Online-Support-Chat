6d78a80cafa5aa5467d874cff0243aa4e2af2c8a
Chris Waters <floridarealty001@gmail.com>
2022-05-04 16:07:35 -0400

    adding to portfolio additional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import LoginButton from './Components/login';
 import LogoutButton from './Components/logout';
 import {useEffect} from 'react';
 import {gapi} from 'gapi-script';
 import Home from './Components/Home';
 import SupportAdmin from './Components/SupportAdmin';
+import Nav from './Components/Nav';
+import Gallery from './Components/Gallery';
+import ContactForm from './Components/Contact';
+import About from './Components/About';
 
 const clientId = "522260929576-gn6staot2clfdhe808nvsi05elqcfhdf.apps.googleusercontent.com"
 
 
+
+
 function App() {
+
+  const [categories] = useState([
+    {
+      name: 'CMS',
+      description: 'Drupal and Wordpress Sites and web apps built using the CMS Drupal',
+    },
+    { name: 'NodeJS', description: 'NodeJS for backend development' },
+    { name: 'PlainScript', description: 'Built on vanilla JS, HTML, CSS' },
+    { name: 'FutureProj', description: 'Projects planned for the future' },
+  ]);
 
   useEffect(() => {
     function start() {
@@ -23,9 +39,34 @@ function App() {
   })
   const path = window.location.pathname
 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+
 
   return (
     <div className="App">
+  <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+
+<main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About />
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
+      </main>
+
+
     <React.StrictMode>
     { path.indexOf('/support') === -1 ? <Home /> : <SupportAdmin /> }
   </React.StrictMode>,
